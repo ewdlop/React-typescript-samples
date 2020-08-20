faaebda47cd06891bd3760f790d4d782abe7b359
ewdlop <ray810815@gmail.com>
2020-08-20 12:02:12 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,22 @@ import ThreeScene from './features/Three/ThreeScene';
 import Box from './features/Three/ThreeSceneFiber';
 import { Canvas } from 'react-three-fiber'
  
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ThreeScene/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
+  <React.StrictMode>
+    <Test/>
+  </React.StrictMode>,
   document.getElementById('root')
-)
+);
+
+// ReactDOM.render(
+//   <Canvas>
+//     <ambientLight />
+//     <pointLight position={[10, 10, 10]} />
+//     <Box position={[-1.2, 0, 0]} />
+//     <Box position={[1.2, 0, 0]} />
+//   </Canvas>,
+//   document.getElementById('root')
+// )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
